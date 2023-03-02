24164eb8da02af0d904c93edaca62a5868feec08
DevinaT <devinat@uw.edu>
2023-03-02 14:03:50 -0800

    added functions for devina's file

--- devina_questions.py
+++ devina_questions.py
@@ -5,41 +5,98 @@ import plotly.express as px
 # load the data
 cardio_vasc = cd.cardiovascular_df
 obesity = cd.obesity_df
+topTenList = []
 
-# filter cardiovascualr
-cardio_vasc = cardio_vasc[cardio_vasc['Data_Value_Alt'] > 0]
 
-# find top 10 states with highest rate of cardiovascular disease
-cardio_vasc_10 = cardio_vasc.groupby('LocationAbbr')['Data_Value_Alt'].mean().nlargest(10)
-#format cardiovascular data for graph
-cardio_vasc = cardio_vasc[(cardio_vasc['LocationAbbr'] == 'WV') |(cardio_vasc['LocationAbbr'] == 'KY') |
-                  (cardio_vasc['LocationAbbr'] == 'IA') | (cardio_vasc['LocationAbbr'] == 'TN') |
-                  (cardio_vasc['LocationAbbr'] == 'AL') | (cardio_vasc['LocationAbbr'] == 'OH') |
-                  (cardio_vasc['LocationAbbr'] == 'AR') | (cardio_vasc['LocationAbbr'] == 'DE') |
-                  (cardio_vasc['LocationAbbr'] == 'WI') | (cardio_vasc['LocationAbbr'] == 'ND')]
-cardio_vasc = cardio_vasc[(cardio_vasc['Year'] >= 2011) & (cardio_vasc['Year'] <= 2018)]
-cardio_vasc = cardio_vasc.groupby(['LocationAbbr', 'Year'])['Data_Value_Alt'].mean().reset_index(name ='Average Cardiovascular Disease Percentage Rate')
-# print(cardio_vasc)
+def find_top_ten(cardio_vasc: pd.dataframe) -> None:
+    """
+    Finds the top 10 states in the dataset
+    with the highest risk of cardiovascular disease.
+    Return a list of the top 10 states.
+    """
+    # filter cardiovascualr
+    cardio_vasc = cardio_vasc[cardio_vasc['Data_Value_Alt'] > 0]
+    # find top 10 states with highest rate of cardiovascular disease
+    cardio_vasc_10 = cardio_vasc.groupby('LocationAbbr')['Data_Value_Alt'].mean()
+    cardio_vasc_10 = cardio_vasc_10.reset_index(name='Percentage')
+    cardio_vasc_10 = cardio_vasc_10.nlargest(10, 'Percentage')
+    topTenList.append(cardio_vasc_10["LocationAbbr"].values.tolist())
 
-# filter obesity dataset
-obesity = obesity[(obesity['Response'] == 'Obese (BMI 30.0 - 99.8)') | (obesity['Response'] == 'Overweight (BMI 25.0-29.9)')]
-obesity = obesity[(obesity['Locationabbr'] == 'WV') |(obesity['Locationabbr'] == 'KY') |
-                  (obesity['Locationabbr'] == 'IA') | (obesity['Locationabbr'] == 'TN') |
-                  (obesity['Locationabbr'] == 'AL') | (obesity['Locationabbr'] == 'OH') |
-                  (obesity['Locationabbr'] == 'AR') | (obesity['Locationabbr'] == 'DE') |
-                  (obesity['Locationabbr'] == 'WI') | (obesity['Locationabbr'] == 'ND')]
-obesity = obesity[(obesity['Year'] >= 2011) & (obesity['Year'] <= 2018)]
 
-# perform 2 groupby operations, first group by location and then year
-obesity = obesity.groupby(['Locationabbr', 'Year'])['Data_value'].mean().reset_index(name ='Average Obesity Percentage Rate')
-# print(obesity)
+def format_cardiovasc(df: pd.dataframe) -> pd.dataframe:
+    """
+    Filters the cardiovasular dataset down to only
+    the states contained within the top 10 list made earlier.
+    Also filters down the dataset to only the years we want
+    (2011-2018). Then, group by states and subgroup by year. Find
+    the mean value of cardiovscular rates for each of these
+    subgroups and rename the column for plotting.
+    """
+    df = df[(df['LocationAbbr'] == topTenList[0]) |
+            (df['LocationAbbr'] == topTenList[1]) |
+            (df['LocationAbbr'] == topTenList[2]) |
+            (df['LocationAbbr'] == topTenList[3]) |
+            (df['LocationAbbr'] == topTenList[4]) |
+            (df['LocationAbbr'] == topTenList[5]) |
+            (df['LocationAbbr'] == topTenList[6]) |
+            (df['LocationAbbr'] == topTenList[7]) |
+            (df['LocationAbbr'] == topTenList[8]) |
+            (df['LocationAbbr'] == topTenList[9])]
+    df = df[(df['Year'] >= 2011) & (df['Year'] <= 2018)]
+    vals = ['LocationAbbr', 'Year']
+    colName = 'Average Cardiovascular Disease Percentage Rate'
+    df = df.groupby(vals)['Data_Value_Alt'].mean()
+    df = df.reset_index(name=colName)
+    return df
 
-# plotting time!
 
-# this is for obesity
-fig = px.line(obesity, x='Year', y='Average Obesity Percentage Rate', color='Locationabbr', markers=True)
-fig.show()
+def format_obesity(obesity: pd.dataframe) -> pd.dataframe:
+    """
+    Filters the obesity dataset down to only
+    the states contained within the top 10 list made earlier.
+    Also filters down the dataset to only the years we want
+    (2011-2018). Then, group by states and subgroup by year. Find
+    the mean value of obesity percentages for each of these
+    subgroups and rename the column for plotting.
+    """
+    # filter obesity dataset
+    obesity = obesity[(obesity['Response'] == 'Obese (BMI 30.0 - 99.8)') |
+                      (obesity['Response'] == 'Overweight (BMI 25.0-29.9)')]
+    obesity = obesity[(obesity['Locationabbr'] == topTenList[0]) |
+                      (obesity['Locationabbr'] == topTenList[1]) |
+                      (obesity['Locationabbr'] == topTenList[2]) |
+                      (obesity['Locationabbr'] == topTenList[3]) |
+                      (obesity['Locationabbr'] == topTenList[4]) |
+                      (obesity['Locationabbr'] == topTenList[5]) |
+                      (obesity['Locationabbr'] == topTenList[6]) |
+                      (obesity['Locationabbr'] == topTenList[7]) |
+                      (obesity['Locationabbr'] == topTenList[8]) |
+                      (obesity['Locationabbr'] == topTenList[9])]
+    obesity = obesity[(obesity['Year'] >= 2011) & (obesity['Year'] <= 2018)]
+    # perform 2 groupby operations, first group by location and then year
+    obesity = obesity.groupby(['Locationabbr', 'Year'])['Data_value'].mean()
+    obesity = obesity.reset_index(name='Average Obesity Percentage Rate')
+    return obesity
 
-#this is for cardiovascular
-fig = px.line(cardio_vasc, x='Year', y='Average Cardiovascular Disease Percentage Rate', color='LocationAbbr', markers=True)
-fig.show()
+
+def main():
+    # Call your test functions here!
+    find_top_ten(cardio_vasc)
+    format_cardiovasc(cardio_vasc)
+    format_obesity(obesity)
+    # plotting time!
+    # this is for obesity
+    fig = px.line(format_obesity(obesity), x='Year',
+                  y='Average Obesity Percentage Rate',
+                  color='Locationabbr', markers=True)
+    fig.show()
+
+    # this is for cardiovascular
+    fig = px.line(format_cardiovasc(cardio_vasc), x='Year',
+                  y='Average Cardiovascular Disease Percentage Rate',
+                  color='LocationAbbr', markers=True)
+    fig.show()
+
+
+if __name__ == '__main__':
+    main()
