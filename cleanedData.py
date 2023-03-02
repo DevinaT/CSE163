d3c01372b1dd3ebfc0e103be917247606f071ebe
clairegl <cglai@uw.edu>
2023-03-02 11:18:15 -0800

    cleaning functions

--- cleanedData.py
+++ cleanedData.py
@@ -1,16 +1,26 @@
+"""
+Niyat Efrem, Claire Lai, Devina Tavahia
+TA Mentor: Palo Pan
+This file contains the functions needed to clean all three datasets
+"""
 import pandas as pd
 
+# cardiovascular_df = pd.read_csv('https://raw.githubusercontent.com/DevinaT/CSE163/main/Cardiovascular.csv')
+# obesity_df = pd.read_csv('https://raw.githubusercontent.com/DevinaT/CSE163/main/Obesity.csv')
+# tabacco_df = pd.read_csv('https://raw.githubusercontent.com/DevinaT/CSE163/main/Tobacco.csv')
 
-# def main():
-cardiovascular_df = pd.read_csv('https://raw.githubusercontent.com/DevinaT/CSE163/main/Cardiovascular.csv')
-obesity_df = pd.read_csv('https://raw.githubusercontent.com/DevinaT/CSE163/main/Obesity.csv')
-tabacco_df = pd.read_csv('https://raw.githubusercontent.com/DevinaT/CSE163/main/Tobacco.csv')
 
-    # tobacco dataset cleaning
-tabacco_df = tabacco_df[['YEAR', 'LocationAbbr', 'TopicDesc',
+def tabacco_clean(file: str) -> pd.DataFrame:
+    '''
+    This function takes in the tabacco dataset and returns a cleaned
+    dataset with only the 50 states and the columns of interest.
+    '''
+    tabacco_df = pd.read_csv(file)
+    tabacco_df = tabacco_df[['YEAR', 'LocationAbbr', 'TopicDesc',
                              'MeasureDesc', 'Data_Value', 'Gender',
                              'Race', 'Age', 'Education']]
-tabacco_df = tabacco_df[(tabacco_df['TopicDesc'] == 'Cigarette Use (Adults)') &
+    tabacco_df = tabacco_df[(tabacco_df['TopicDesc'] ==
+                            'Cigarette Use (Adults)') &
                             (tabacco_df['MeasureDesc'] == 'Current Smoking') &
                             (tabacco_df['LocationAbbr'] != 'PR') &
                             (tabacco_df['LocationAbbr'] != 'GU') &
@@ -23,18 +33,33 @@ tabacco_df = tabacco_df[(tabacco_df['TopicDesc'] == 'Cigarette Use (Adults)') &
                             (tabacco_df['YEAR'] != '2012-2013') &
                             (tabacco_df['YEAR'] != '2014-2015') &
                             (tabacco_df['YEAR'] != '2015-2016')]
+    return tabacco_df
 
-    # obesity dataset cleaning lol
-is_state = ((obesity_df["Locationdesc"] != "Puerto Rico") &
-            (obesity_df["Locationdesc"] != "Virgin Islands") &
-            (obesity_df["Locationdesc"] != "District of Columbia") &
-            (obesity_df["Locationdesc"] != "Guam"))
-obesity_df = obesity_df.loc[is_state, ["Year", "Locationabbr",
-                                        "Response", "Break_Out",
-                                        "Data_value", "GeoLocation"]]
 
-    # cardiovascular dataset cleaning
-cardiovascular_df = cardiovascular_df[(cardiovascular_df['LocationAbbr'] != 'DC') & (cardiovascular_df['LocationAbbr'] != 'USM')]
+def obesity_cleaned(filename: str) -> pd.DataFrame:
+    """
+    This function takes in the obesity dataframe and filters out all the
+    locations that are not the 50 US states. For these states it returns
+    only the relevant columns to our anaylsis.
+    """
+    obesity_df = pd.read_csv(filename)
+    is_state = ((obesity_df["Locationdesc"] != "Puerto Rico") &
+                (obesity_df["Locationdesc"] != "Virgin Islands") &
+                (obesity_df["Locationdesc"] != "District of Columbia") &
+                (obesity_df["Locationdesc"] != "Guam"))
+    obesity_df = obesity_df.loc[is_state, ["Year", "Locationabbr",
+                                           "Response", "Break_Out",
+                                           "Data_value", "GeoLocation"]]
+    return obesity_df
 
-# if __name__ == '__main__':
-#     main()
+
+def cardiovascular_cleaned(filename: str) -> pd.DataFrame:
+    """
+    This function takes in the cardiovascular dataframe and filters out all the
+    locations that are not the 50 US states. For these states it returns
+    only the relevant columns to our anaylsis.
+    """
+    c_df = pd.read_csv(filename)
+    c_df = c_df[(c_df['LocationAbbr'] != 'DC') &
+                (c_df['LocationAbbr'] != 'USM')]
+    return c_df
