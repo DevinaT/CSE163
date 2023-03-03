7fc73acbdef62165d26c7d35b19bb2f71c561176
clairegl <cglai@uw.edu>
2023-03-03 13:16:36 -0800

    updated obesity tests

--- projects_utils.py
+++ projects_utils.py
@@ -3,6 +3,59 @@ Niyat Efrem, Claire Lai, Devina Tavahia
 TA Mentor: Palo Pan
 This file contains the assert_equals function needed to write tests. 
 """
+from typing import Any
+import numpy as np
+import pandas as pd
+import math
+
+TOLERANCE = 0.001
+
+
+def check_approx_equals(expected: Any, received: Any) -> bool:
+    """
+    Checks received against expected, and returns whether or
+    not they match (True if they do, False otherwise).
+    If the argument is a float, will do an approximate check.
+    If the arugment is a data structure will do an approximate check
+    on all of its contents.
+    """
+    try:
+        if type(expected) == dict:
+            # first check that keys match, then check that the
+            # values approximately match
+            return expected.keys() == received.keys() and \
+                all([check_approx_equals(expected[k], received[k])
+                    for k in expected.keys()])
+        elif type(expected) == list or type(expected) == set:
+            # Checks both lists/sets contain the same values
+            return len(expected) == len(received) and \
+                all([check_approx_equals(v1, v2)
+                     for v1, v2 in zip(expected, received)])
+        elif type(expected) == float:
+            return math.isclose(expected, received, abs_tol=TOLERANCE)
+        elif type(expected) == np.ndarray:
+            return np.allclose(expected, received, atol=TOLERANCE,
+                               equal_nan=True)
+        elif type(expected) == pd.DataFrame:
+            try:
+                pd.testing.assert_frame_equal(expected, received,
+                                              atol=TOLERANCE)
+                return True
+            except AssertionError:
+                return False
+        elif type(expected) == pd.Series:
+            try:
+                pd.testing.assert_series_equal(expected, received,
+                                               atol=TOLERANCE)
+                return True
+            except AssertionError:
+                return False
+        else:
+            return expected == received
+    except Exception as e:
+        print(f"EXCEPTION: Raised when checking check_approx_equals {e}")
+        return False
+
 
 def assert_equals(expected: Any, received: Any) -> None:
     """
--- test_file.py
+++ test_file.py
@@ -1,23 +1,31 @@
 import cleanedData as cd
 import pandas as pd
 from projects_utils import assert_equals
+import numpy as np
 
 # unfiltered_test_tabacco = ("https://raw.githubusercontent.com/DevinaT/CSE163/main/test_tabacco.csv")
 # filtered_test_tabacco = 
+
+TEST_OBESITY_RESULT = pd.read_csv("https://raw.githubusercontent.com/DevinaT/CSE163/main/obesity_test_clean.csv")
 
 
 # def test_tabacco_clean(test_file: str) -> None:
     # assert_equals(filtered_test_df, cd.tabacco_clean(unfiltered_test_tabacco))
 
-def test_obsity_clean(test_file: str, result_file: str) -> None:
-    assert_equals(result_file, cd.obesity_cleaned(test_file))
+def test_obesity_clean(test_file: str, result_file: str) -> None:
+    clean_obesity_test = cd.obesity_cleaned(test_file)
+    assert_equals(len(result_file), len(clean_obesity_test))
+    assert_equals(result_file, clean_obesity_test)
     print("test_obsity_clean passed")
-
 
 
 def main():
     # test_file = pd.read_csv(unfiltered_test_tabacco)
     # filtered_test_df
-    test_obsity_clean()
+
+    test_obesity_clean("https://raw.githubusercontent.com/DevinaT/CSE163/main/obesity_test.csv",
+                       TEST_OBESITY_RESULT)
 
 
+if __name__ == "__main__":
+    main()
